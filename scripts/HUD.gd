b3fac78bfbfd402b06fdd83277d38982dc58fdff
Marcelo <marceloalvessoft@gmail.com>
2020-02-02 00:26:57 -0300

    Hud

--- scripts/HUD.gd
+++ scripts/HUD.gd
@@ -15,4 +15,16 @@ func _ready():
 func _process(delta):
 
 	$HBoxContainer/VBoxContainer/HP2/PlayerSword.value =player.sword_life
+	$HBoxContainer/time.text =  str(round($Timer.time_left))
+	$HBoxContainer/MarginContainer/HBoxContainer/metal.text =  str(round(player.quant_item))
+	
+	if (round($Timer.time_left) == 10):
+		$HBoxContainer/time.add_color_override("font_color", Color(1,0,0,1))
+	
 
+func _on_Timer_ready():
+	print("d")
+
+
+func _on_Timer_timeout():
+	$HBoxContainer/time.add_color_override("font_color", Color(1,1,1,1))
