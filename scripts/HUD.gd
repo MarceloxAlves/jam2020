473474d4901fbe6816c845394a223c4b97df05b6
jesusadadfilho <jesusadadfilho@gmail.com>
2020-02-01 23:23:52 -0300

    Fix bug

--- scripts/HUD.gd
+++ scripts/HUD.gd
@@ -1,17 +1,13 @@
 extends MarginContainer
 
+onready var player = get_parent().get_node('Player')
+onready var timer = $Timer
 
-# Declare member variables here. Examples:
-# var a = 2
-# var b = "text"
-onready var  player =   get_parent().get_node('Player')
-
-# Called when the node enters the scene tree for the first time.
 func _ready():
 	set_process(true)
+	timer.wait_time += 5
 
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 
 	$HBoxContainer/VBoxContainer/HP2/PlayerSword.value =player.sword_life
@@ -28,3 +24,5 @@ func _on_Timer_ready():
 
 func _on_Timer_timeout():
 	$HBoxContainer/time.add_color_override("font_color", Color(1,1,1,1))
+	self.get_parent().horda_add()
+	timer.wait_time += 5
