c3f94af2fa87cd3b82a1d6843cb4b24868cac2b8
Marcelo <marceloalvessoft@gmail.com>
2020-02-02 03:30:21 -0300

    Tudo normal

--- scripts/HUD.gd
+++ scripts/HUD.gd
@@ -12,7 +12,8 @@ func _process(delta):
 
 	$HBoxContainer/VBoxContainer/HP2/PlayerSword.value =player.sword_life
 	$HBoxContainer/time.text =  str(round($Timer.time_left))
-	$HBoxContainer/MarginContainer/HBoxContainer/metal.text =  str(round(player.quant_item))
+	$HBoxContainer/HBoxContainer/metal.text =  str(round(player.quant_item))
+
 	
 	if (round($Timer.time_left) == 10):
 		$HBoxContainer/time.add_color_override("font_color", Color(1,0,0,1))
--- scripts/Item.gd
+++ scripts/Item.gd
@@ -1,11 +1,37 @@
 extends Node2D
 
-func _ready():
-	pass
+var collected = false
+onready var Hitem = get_parent().get_node_or_null('HUD')
+var positionNext = Vector2(0,0)
+var energy_max = 0.3
 
+func _ready():
+	positionNext =  Vector2(get_viewport().get_visible_rect().size.x - 50, 50)
+	set_process(true)
+	$Tween.interpolate_property($Light2D, 'energy', null, energy_max, 0.3, Tween.TRANS_CUBIC, Tween.EASE_OUT)
+	$Tween.start()
+		
+	
+func _process(delta):
+	if collected:
+		self.position =  	self.position.linear_interpolate(positionNext, delta * 4)
+		if self.position.distance_to(positionNext) < 100:
+			queue_free()
+		
 func _on_Area2D_body_entered(body):
 	if (body.name == "Player"):
 		body.pick_up_item()
-		queue_free()
+		collected = true
+		#queue_free()
 		
 	
+
+
+func _on_Tween_tween_all_completed():
+	$Tween2.interpolate_property($Light2D, 'energy', null, 0, 0.3, Tween.TRANS_CUBIC, Tween.EASE_OUT)
+	$Tween2.start()
+
+
+func _on_Tween2_tween_all_completed():
+	$Tween.interpolate_property($Light2D, 'energy', null, energy_max, 0.3, Tween.TRANS_CUBIC, Tween.EASE_OUT)
+	$Tween.start()
